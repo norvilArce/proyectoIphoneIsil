5be684e3dab0f0107d53b2a2fe551423741bdbac
Norvil <norvil.arce@gmail.com>
2020-11-08 22:22:41 -0800

    agrego ocultar teclado en el login

--- ProyectoIphoneIsil/ViewController.swift
+++ ProyectoIphoneIsil/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func clickBtnCloseKeyboard(_ sender: Any) {
+        self.view.endEditing(true)
+        }
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+           super.viewDidLoad()
+           
+           NotificationCenter.default.addObserver(self,
+                                                  selector: #selector(self.keyboardWillShow(_:)),
+                                                  name: UIResponder.keyboardWillShowNotification,
+                                                  object: nil)
+           
+           NotificationCenter.default.addObserver(self,
+                                                  selector: #selector(self.keyboardWillHide(_:)),
+                                                  name: UIResponder.keyboardWillHideNotification,
+                                                  object: nil)
+       }
+       
+       @objc func keyboardWillShow(_ notification: Notification) {
+           print("EL TECLADO APARECE")
+           
+           let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+           let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+           
+           print(keyboardFrame)
+           print(animationDuration)
+       }
+       
+       @objc func keyboardWillHide(_ notification: Notification) {
+           print("El TECLADO SE OCULTA")
+       }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.view.endEditing(true)
+        }
 
 }
 
